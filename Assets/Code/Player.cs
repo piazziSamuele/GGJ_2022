4e5405b9c3c07f5460aab1349210b163605445a7
Matteo Carlo Rossi <31008136+RoninCreativeStudio@users.noreply.github.com>
2022-01-25 23:20:51 +0000

    shit this is fun

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -6,10 +6,12 @@ public class Player : MonoBehaviour
 {
     public bool IsAI;
     private Material m_playerMat;
+    private PlayerMovement m_movememnt;
     // Start is called before the first frame update
     void Start()
     {
         m_playerMat = GetComponent<MeshRenderer>().sharedMaterial;
+        m_movememnt = GetComponent<PlayerMovement>();
     }
 
     // Update is called once per frame
@@ -21,7 +23,7 @@ public class Player : MonoBehaviour
     public void Switch()
     {
         IsAI = !IsAI;
-
+        if (m_movememnt != null) m_movememnt.enabled = !m_movememnt.enabled;
         m_playerMat.SetColor("_Tint", IsAI ? Color.red : Color.blue);
     }
 }
