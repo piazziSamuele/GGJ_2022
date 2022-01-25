3ecef5711f565c713ad7c0d0beb6102bbe022c1b
Matteo Carlo Rossi <31008136+RoninCreativeStudio@users.noreply.github.com>
2022-01-25 03:00:17 +0000

    env 1st iteration

---
+++ Assets/Code/EnvManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvManager : MonoBehaviour
+{
+    private AudioSpectrum spectrum;
+    private Material arenaMat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spectrum = GameMatchManager.Manager.spectrum;
+        arenaMat = GetComponent<MeshRenderer>().sharedMaterial;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (GameMatchManager.Manager.soundtrack != null && GameMatchManager.Manager.soundtrack.isPlaying)
+        {
+            Color newArenaTint = new Color(Mathf.Clamp01( spectrum.MeanLevels[0] * 15f), Mathf.Clamp01(spectrum.MeanLevels[1] * 15f), Mathf.Clamp01(spectrum.MeanLevels[2] * 15f), 1.0f);
+            arenaMat.SetColor("_Tint", newArenaTint);
+
+        }
+    }
+}
