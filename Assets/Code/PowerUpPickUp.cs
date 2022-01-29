06801317314c1ccebea9ebf87a13ebbac90f4f98
RoninCreativeStudio <31008136+RoninCreativeStudio@users.noreply.github.com>
2022-01-29 15:26:37 +0100

    Gizmos

--- Assets/Code/PowerUpPickUp.cs
+++ Assets/Code/PowerUpPickUp.cs
@@ -5,5 +5,18 @@ using UnityEngine;
 public class PowerUpPickUp : MonoBehaviour
 {
     public GenericPowerUp powerUpPrefab;
-    
+    private float m_radius = .5f;
+
+    private void Start()
+    {
+        m_radius = GetComponent<SphereCollider>().radius;
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, m_radius);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, m_radius * 2.0f);
+    }
 }
